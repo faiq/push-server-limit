85d5f2ad949c3d043e42fd84f69c1754a36a555b
faiq <faiqrazarizvi@gmail.com>
2014-11-04 16:38:49 -0500

    works, kinda

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var redback = require('redback').createClient()
+var redis = require('redis').createClient()
+  , redback = require('redback').use(redis)
 
 module.exports = function (options) { 
   return function (req, res, next) { 
@@ -9,12 +10,16 @@ module.exports = function (options) {
         subject_expiry: 200
       }
     } 
+    console.log('b4 creating redis thing') 
     var rateLimit = redback.createRateLimit('requests', options)
       , ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    console.log('after redis thing')
     rateLimit.add(ip) 
     rateLimit.count(ip, 20, function (err, count) { 
-      if (count > 1) res.end('tooo many') 
-      else next() 
+      if (count > 1) { 
+        console.log('more dan 1')
+        res.end('tooo many') 
+      } else next() 
     })
   }
 } 
