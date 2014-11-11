e5a419b8bf46322852ff74bb00dc5a052ea8fb66
faiq <faiqrazarizvi@gmail.com>
2014-11-10 19:05:58 -0500

    makes an object out of middleware

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require('express')
   , http = require('http')
-  , rateLimit = require('./index') 
   , app = express() 
   , server = http.createServer(app)
 
-app.use(rateLimit()) 
+app.use(function(req, res, next) {
+  console.log(req.originalUrl)
+})
+app.use(function (req, res, next) { 
+  next(new Error('some redis shit')) 
+})
+
 app.get('/', function (req, res) { 
   res.send('heres a response') 
 })
+
+app.use(function (e, req, res, n){ 
+  console.log(e + ' error was thrown') 
+})
+
 server.listen(3000)
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require("assert")
+  , middleware = require("../lib/middleware.js")
+
+describe("the middleware", function (done) {
+  it ("should make default options, when nothing is passed in", function (done) { 
+    done()    
+  })
+})
